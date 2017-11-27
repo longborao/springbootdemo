42573c80d1871d5e281654636d3e1ed4e42434fa
raolongbo <714759009@qq.com>
2017-11-27 15:58:51 +0800

    first commit

---
+++ RestExample/src/main/java/com/panodata/restful/atom/AtomApis.java
@@ -0,0 +1,13 @@
+package com.panodata.restful.atom;
+
+import com.panodata.conf.base.BaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by robot on 2017/10/10.
+ */
+@RestController
+public class AtomApis extends BaseController{
+
+
+}
